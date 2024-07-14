66cf76b3bac6931bcf6e8cea29d2fdd94a42871b
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-14 13:59:08 +0530

    Generated: `advanced-ignore-channel-bot/src/utils/constants.js`

---
+++ advanced-ignore-channel-bot/src/utils/constants.js
@@ -0,0 +1,27 @@
+export const DiscordConstants = {
+  API_URL: 'https://discord.com/api/v9',
+  BOT_TOKEN: process.env.BOT_TOKEN,
+};
+
+export const IgnoreConstants = {
+  TOGGLE_IGNORE: 'toggle_ignore',
+  ADD_CHANNEL: 'add_channel',
+  REMOVE_CHANNEL: 'remove_channel',
+};
+
+export const NotificationConstants = {
+  IGNORED_CHANNEL_MESSAGE: 'You have chosen to ignore this channel.',
+  NOTIFICATION_SETTINGS_UPDATED: 'Notification settings updated successfully.',
+};
+
+export const ServerConstants = {
+  MULTIPLE_SERVER_SUPPORT: true,
+};
+
+export const AdvancedIgnoreOptions = {
+  MUTE_KEYWORDS: true,
+};
+
+export const UserInterfaceConstants = {
+  USER_FRIENDLY_INTERFACE: true,
+};
